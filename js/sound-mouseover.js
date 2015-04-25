4a7809b22ae73becc31cdc62ab898d93a0f4ac44
sunnyyy <coconutcandy12@gmail.com>
2015-04-25 16:11:56 -0400

    adding web files

---
+++ js/sound-mouseover.js
@@ -0,0 +1,143 @@
+<!--
+function playclip1() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "one.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClip1"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
+
+<!--
+function playclip2() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "two.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClip2"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
+
+<!--
+function playclip3() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "three.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClip3"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
+
+<!--
+function playclip4() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "four.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClip4"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
+
+<!--
+function playclip5() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "five.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClip5"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
+
+<!--
+function playclip6() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "six.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClip6"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
+
+<!--
+function playclip7() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "seven.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClip7"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
+
+<!--
+function playclip8() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "eight.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClip8"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
+
+<!--
+function playclip9() {
+	if (navigator.appName == "Microsoft Internet Explorer") {
+		if (document.all) {
+			document.all.sound.src = "penta.mp3";
+		}
+	} else {
+		{
+			var audio = document.getElementById("soundClipPenta"); audio.play();
+			audio.play();
+		}
+	}
+}
+
+//-->
